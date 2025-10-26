98c430cf1515e8f2372c57c8ba0cd9a7020012f8
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-10-26 16:29:15 +0530

    feat: keep redis alive
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import express, { Request, Response, Express } from 'express'
 import cors from 'cors'
+import express, { Request, Response, Express } from 'express'
 
 import authRoutes from './routes/auth.js'
 import blogRoutes from './routes/blogs.js'
 
-import { errorHandler, notFoundHandler } from './middleware/errorHandler.js'
 import { startCleanupJob } from './utils/scheduledJobs.js'
+import { errorHandler, notFoundHandler } from './middleware/errorHandler.js'
 
 const app: Express = express()
 const PORT = process.env.PORT || 3000
@@ -35,6 +35,20 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/health', (_req: Request, res: Response) => {
   res.json({ status: 'ok', message: 'Blog Backend API is running' })
+})
+
+import { Redis } from '@upstash/redis'
+
+app.get("/redis-test", async (_req: Request, res: Response) => {
+  const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  })
+  
+  await redis.set("foo","bar");
+  await redis.get("foo");
+  await redis.del("foo");
+  res.json({ status: 'ok', message: 'Redis test successful' })
 })
 
 app.use('/api/auth', authRoutes)
